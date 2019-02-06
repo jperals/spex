7b1eabf2bc3e08b810c1a086fc748380c2cea2a2
Joan Perals <joan.perals@ixds.com>
2019-02-06 11:49:11 +0100

    Change file indentation

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -24,167 +24,167 @@
 </template>
 
 <style scoped lang="scss">
-  .TopBar {
+.TopBar {
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  border-bottom: 1px solid #c6c8c9;
+  width: 100%;
+  height: 64px;
+  overflow: inherit;
+
+  .BackIcon {
+    width: 24px;
+    height: 24px;
+    background-image: url("../assets/icons/back.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .Components {
+    justify-content: flex-end;
+    width: 24px;
+    height: 24px;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    background-image: url("../assets/icons/components.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  .PageTitle {
+    font-size: 20px;
+    color: #031b26;
+    line-height: 32px;
+    vertical-align: middle;
+    margin-top: 16px;
+    margin-bottom: 16px;
+  }
+
+  .StoryIcon {
+    justify-content: flex-end;
+    width: 24px;
+    height: 24px;
+    margin: auto;
+    background-image: url("../assets/icons/story_Focus.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .SystemIcon {
+    justify-content: flex-end;
+    width: 24px;
+    height: 24px;
+    margin: auto;
+    background-image: url("../assets/icons/system.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .Divider {
+    justify-content: flex-end;
+    width: 1px;
+    height: 24px;
+    margin: auto;
+    background-image: url("../assets/icons/divider.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-left: 4px;
+    margin-right: 4px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  /* Tooltip container */
+  .tooltip {
+    position: relative;
+    display: inline-block;
+  }
+
+  /* Tooltip text */
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    margin-left: -32px;
+    background-color: #707679;
+    color: #fff;
+    font-weight: 200;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+
+    /* Position the tooltip text - see examples below! */
+    position: absolute;
+    z-index: 1;
+  }
+
+  /* Show the tooltip text when you mouse over the tooltip container */
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
+  }
+
+  .tooltip .tooltiptext::after {
+    content: " ";
+    position: absolute;
+    bottom: 100%; /* At the top of the tooltip */
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent transparent #707679 transparent;
+  }
+
+  .link-back {
     display: flex;
-    flex-direction: row;
-    background-color: white;
-    border-bottom: 1px solid #c6c8c9;
-    width: 100%;
-    height: 64px;
-    overflow: inherit;
-
-    .BackIcon {
-      width: 24px;
-      height: 24px;
-      background-image: url("../assets/icons/back.png");
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin-left: 16px;
-      margin-right: 16px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-    }
-
-    .Components {
-      justify-content: flex-end;
-      width: 24px;
-      height: 24px;
-      margin-left: 16px;
-      margin-right: 16px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-      background-image: url("../assets/icons/components.png");
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-
-    .PageTitle {
-      font-size: 20px;
-      color: #031b26;
-      line-height: 32px;
-      vertical-align: middle;
-      margin-top: 16px;
-      margin-bottom: 16px;
-    }
-
-    .StoryIcon {
-      justify-content: flex-end;
-      width: 24px;
-      height: 24px;
-      margin: auto;
-      background-image: url("../assets/icons/story_Focus.png");
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin-left: 16px;
-      margin-right: 16px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-    }
-    .SystemIcon {
-      justify-content: flex-end;
-      width: 24px;
-      height: 24px;
-      margin: auto;
-      background-image: url("../assets/icons/system.png");
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin-left: 16px;
-      margin-right: 16px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-    }
-    .Divider {
-      justify-content: flex-end;
-      width: 1px;
-      height: 24px;
-      margin: auto;
-      background-image: url("../assets/icons/divider.png");
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin-left: 4px;
-      margin-right: 4px;
-      margin-top: 20px;
-      margin-bottom: 20px;
-    }
-
-    /* Tooltip container */
-    .tooltip {
-      position: relative;
-      display: inline-block;
-    }
-
-    /* Tooltip text */
-    .tooltip .tooltiptext {
-      visibility: hidden;
-      width: 120px;
-      margin-left: -32px;
-      background-color: #707679;
-      color: #fff;
-      font-weight: 200;
-      text-align: center;
-      padding: 5px 0;
-      border-radius: 6px;
-
-      /* Position the tooltip text - see examples below! */
-      position: absolute;
-      z-index: 1;
-    }
-
-    /* Show the tooltip text when you mouse over the tooltip container */
-    .tooltip:hover .tooltiptext {
-      visibility: visible;
-    }
-
-    .tooltip .tooltiptext::after {
-      content: " ";
-      position: absolute;
-      bottom: 100%; /* At the top of the tooltip */
-      left: 50%;
-      margin-left: -5px;
-      border-width: 5px;
-      border-style: solid;
-      border-color: transparent transparent #707679 transparent;
-    }
-
-    .link-back {
-      display: flex;
-      flex-grow: 1;
-      &:not(:hover) {
-        text-decoration: none;
-      }
+    flex-grow: 1;
+    &:not(:hover) {
+      text-decoration: none;
     }
   }
+}
 </style>
 
 <script>
-  import SuggestionsIndicator from "@/components/SuggestionsIndicator";
+import SuggestionsIndicator from "@/components/SuggestionsIndicator";
 
-  export default {
-    name: "top-bar",
-    components: {SuggestionsIndicator},
-    props: {
-      numberOfSuggestions: {
-        type: Number,
-        default: 0
-      },
-      story: {
-        type: Object
-      }
+export default {
+  name: "top-bar",
+  components: {SuggestionsIndicator},
+  props: {
+    numberOfSuggestions: {
+      type: Number,
+      default: 0
     },
-    computed: {
-      backUrl() {
-        return this.story ?
-          '/story/' + this.story.id
-          :
-          this.$route.fullPath
-      }
+    story: {
+      type: Object
     }
-  };
+  },
+  computed: {
+    backUrl() {
+      return this.story ?
+        '/story/' + this.story.id
+        :
+        this.$route.fullPath
+    }
+  }
+};
 </script>
 
 <docs>
