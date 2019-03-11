e780baa974ab6d11d987d2d5cdf2d5764f927171
Joan Perals <joan.perals@ixds.com>
2019-03-11 22:59:55 +0100

    Some dummy data for diagrams store

--- src/generate-dummy-data.js
+++ src/generate-dummy-data.js
@@ -5,3 +5,17 @@ store.commit('updateProjects', [
     id: '0'
   }
 ])
+
+store.commit('addDiagramRelationship', {
+  id: '0',
+  from: 'F4lSsfNGpLZlgOnZJcor',
+  to: 'g4LLTzHTrP85FtTLADNN',
+})
+
+store.commit('updateDiagramComponentPosition', {
+  component: {
+    id: 'F4lSsfNGpLZlgOnZJcor'
+  },
+  x: 20,
+  y: 50
+})
