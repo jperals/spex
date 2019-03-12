5fce2deb728ad266b6f68fad118505e1adc9ac83
Joan Perals <joan.perals@ixds.com>
2019-03-12 14:14:47 +0100

    Work around CPU performance issue by using addEventlistener vs v-on:drag*

--- src/components/DiagramComponent.vue
+++ src/components/DiagramComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="diagram-component" :title="component.name" :style="componentStyle(component)" v-on:drag="drag" v-on:dragend="dragend" v-on:dragstart="dragstart">
+  <div class="diagram-component" :title="component.name" draggable="true">
     <label class="component-name">{{component.name}}</label>
     <img v-if="component.imageUrl" :src="component.imageUrl">
   </div>
@@ -85,13 +85,14 @@ export default {
         x: this.initialCoords.x + this.currentDrag.x,
         y: this.initialCoords.y + this.currentDrag.y
       }
+      this.updateElementPosition()
     },
     dragend(event) {
       this.drag(event)
       store.dispatch('moveDiagramComponentPosition', {
         component: this.component,
         delta: this.currentCoords
-      })
+      }).then(this.updateElementPosition)
       this.initialDragCoords = null
       this.initialCoords = this.currentCoords
     },
@@ -101,8 +102,22 @@ export default {
         y: event.y
       }
       this.currentCoords = this.initialDragCoords
+    },
+    updateElementPosition() {
+      const style = this.componentStyle()
+      Object.assign(this.$el.style, style)
     }
+  },
+  mounted() {
+    this.$el.addEventListener('dragstart', event => {
+      this.dragstart(event)
+    })
+    this.$el.addEventListener('drag', event => {
+      this.drag(event)
+    })
+    this.$el.addEventListener('dragend', event => {
+      this.dragend(event)
+    })
   }
-
 }
 </script>
