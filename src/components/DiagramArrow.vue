b8e0179116b89b28ca32e47e3791be2880d6a0de
Joan Perals <joan.perals@ixds.com>
2019-03-15 00:11:37 +0100

    Refactor part of DiagramRelationship component into separate file so that we can reuse the arrow passing it bare coordinates instead of diagram items

---
+++ src/components/DiagramArrow.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="diagram-arrow" :style="arrowStyle">
+    <div class="arrow-line" :style="lineStyle"></div>
+    <div class="arrow-tip" :style="arrowTipStyle">
+      <div class="arrow-line arrow-stroke arrow-stroke-1"></div>
+      <div class="arrow-line arrow-stroke arrow-stroke-2"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.diagram-arrow {
+  position: absolute;
+  left: 0;
+  right: 0;
+  transform-origin: left center;
+  width: 0;
+  height: 0;
+  overflow: visible;
+}
+
+.arrow-line {
+  background-color: #979C9E;
+  transform-origin: left center;
+  width: 1px;
+  height: 1px;
+  position: absolute;
+  top: 0;
+  &.arrow-stroke {
+    transform-origin: right center;
+  }
+  &.arrow-stroke-1 {
+    transform: rotate(20deg) scaleX(8);
+  }
+  &.arrow-stroke-2 {
+    transform: rotate(-20deg) scaleX(8);
+  }
+}
+</style>
+
+<script>
+const strokeWidth = 1
+export default {
+  name: 'diagram-arrow',
+  props: {
+    from: {
+      type: Object
+    },
+    to: {
+      type: Object
+    },
+    distanceBefore: {
+      type: Number,
+      default: 0
+    },
+    distanceAfter: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    arrowTipStyle() {
+      return {
+        transform: `translateX(${this.lineLength}px)`
+      }
+    },
+    distance() {
+      return Math.sqrt(this.width * this.width + this.height * this.height)
+    },
+    width() {
+      return Math.abs(this.to.x - this.from.x)
+    },
+    height() {
+      return Math.abs(this.to.y - this.from.y)
+    },
+    lineLength() {
+      return this.distance - this.distanceAfter
+    },
+    lineStyle() {
+      let scaleFactor = (this.lineLength - this.distanceBefore) / strokeWidth
+      scaleFactor = Math.max(0, scaleFactor)
+      return {
+        transform: `translateX(${this.distanceBefore}px) scaleX(${scaleFactor})`
+      }
+    },
+    rotationInDegrees() {
+      return this.rotationInRadians * 180 / Math.PI
+    },
+    rotationInRadians() {
+      let angle = Math.atan((this.to.y - this.from.y) / (this.to.x - this.from.x))
+      if (this.to.x < this.from.x) {
+        angle += Math.PI
+      }
+      return angle
+    },
+    arrowStyle() {
+      return {
+        transform: `translate(${this.from.x}px, ${this.from.y}px) rotate(${this.rotationInDegrees}deg)`
+      }
+    }
+  }
+}
+</script>
--- src/components/DiagramRelationship.vue
+++ src/components/DiagramRelationship.vue
@@ -1,48 +1,10 @@
 <template>
-  <div class="diagram-relationship" :style="relationshipStyle">
-    <div class="line" :style="lineStyle"></div>
-    <div class="arrow-tip" :style="arrowTipStyle">
-      <div class="line arrow-stroke arrow-stroke-1"></div>
-      <div class="line arrow-stroke arrow-stroke-2"></div>
-    </div>
-  </div>
+  <DiagramArrow :from="position1" :to="position2" :distance-before="100" :distance-after="50"></DiagramArrow>
 </template>
 
-<style lang="scss" scoped>
-.diagram-relationship {
-  position: absolute;
-  left: 0;
-  right: 0;
-  transform-origin: left center;
-  width: 0;
-  height: 0;
-  overflow: visible;
-  .line {
-    background-color: #979C9E;
-    transform-origin: left center;
-    width: 1px;
-    height: 1px;
-    position: absolute;
-    top: 0;
-    &.arrow-stroke {
-      transform-origin: right center;
-    }
-    &.arrow-stroke-1 {
-      transform: rotate(20deg) scaleX(8);
-    }
-    &.arrow-stroke-2 {
-      transform: rotate(-20deg) scaleX(8);
-    }
-  }
-}
-</style>
-
 <script>
+import DiagramArrow from '@/components/DiagramArrow'
 import store from '@/store'
-
-const strokeWidth = 1
-const blankSpaceBetweenFirstItemAndLine = 100
-const blankSpaceBetweenLineAndSecondItem = 50
 
 export default {
   name: 'diagram-relationship',
@@ -51,52 +13,16 @@ export default {
       type: Object
     }
   },
+  components: {
+    DiagramArrow
+  },
   computed: {
-    arrowTipStyle() {
-      return {
-        transform: `translateX(${this.lineLength}px)`
-      }
-    },
-    distance() {
-      return Math.sqrt(this.width * this.width + this.height * this. height)
-    },
     position1() {
       return store.getters.diagramItemById(this.relationship.from.itemId).position
     },
     position2() {
       return store.getters.diagramItemById(this.relationship.to.itemId).position
     },
-    width() {
-      return Math.abs(this.position2.x - this.position1.x)
-    },
-    height() {
-      return Math.abs(this.position2.y - this.position1.y)
-    },
-    lineLength() {
-      return this.distance - blankSpaceBetweenLineAndSecondItem
-    },
-    lineStyle() {
-      let scaleFactor = (this.lineLength - blankSpaceBetweenFirstItemAndLine) / strokeWidth
-      scaleFactor = Math.max(0, scaleFactor)
-      return {
-        transform: `translateX(${blankSpaceBetweenFirstItemAndLine}px) scaleX(${scaleFactor})`
-      }
-    },
-    rotationInDegrees() {
-      return this.rotationInRadians * 180 / Math.PI
-    },
-    rotationInRadians() {
-      let angle = Math.atan((this.position2.y - this.position1.y) / (this.position2.x - this.position1.x))
-      if (this.position2.x < this.position1.x) {
-        angle += Math.PI
-      }
-      return angle
-    },
-    relationshipStyle() {
-      return {
-        transform: `translate(${this.position1.x}px, ${this.position1.y}px) rotate(${this.rotationInDegrees}deg)`
-      }
-    }
   }
 }
 </script>
